ea2a36b17fd3a75c4ec178675ce072909811f324
Matias Korhonen <matias@kiskolabs.com>
2012-06-19 19:01:35 +0300

    Try to fix the thing

--- bin/start_monit.sh
+++ bin/start_monit.sh
@@ -2,4 +2,4 @@
 
 sed -i -e "s/HEROKU_PORT/$PORT/g" ./conf/monitrc
 
-./monit -c ./conf/monitrc -p ./tmp/.monit.pid -s ./tmp/.monit.state -I
+./bin/monit -c ./conf/monitrc -p ./tmp/.monit.pid -s ./tmp/.monit.state -I
