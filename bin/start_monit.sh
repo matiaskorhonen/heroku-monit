6d05227ad1bd0eb07d1ec1948f82656dd1e2b064
Matias Korhonen <matias@kiskolabs.com>
2012-07-14 20:54:59 +0300

    Set a default system name

--- bin/start_monit.sh
+++ bin/start_monit.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export SYSTEM_NAME=${1:-Heroku}
+
 (cat ./conf/monitrc | vendor/gettext/bin/envsubst) > tempfile && mv tempfile ./conf/monitrc
 
 vendor/monit/bin/monit -c ./conf/monitrc -p ./tmp/.monit.pid -s ./tmp/.monit.state -I
